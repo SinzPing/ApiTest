506108d6e61c9ecdc52103705318d2ebe7c70c2f
SinzPing <ping19981226@gmail.com>
2020-06-29 16:39:51 +0800

    修改优化接口测试框架

---
+++ case/testGeneralAffairs/testAssetAdmin/testBuyAdmin/testGetAssetApplyId.py
@@ -0,0 +1,39 @@
+"""
+获取采购申请单号
+author：郑平
+"""
+
+from common import function, tools
+import env
+import unittest
+
+config = tools.choose_env(env.env)
+
+
+class GetAssetApplyId(unittest.TestCase):
+    """
+    获取采购申请单号
+    """
+
+    def setUp(self):
+        print('====TestStart====')
+        token = tools.get_usertoken(config.fdadmin)
+        req = function.ApiRequest
+        req.headers.update({'FDToken': token, 'Content-Type': 'application/json; charset=utf-8'})
+
+    def test_case(self):
+        api = '/educiotlogistics/purchasing-entry/getId'
+        rep = tools.request(api, '')
+        print(rep)
+        log = function.Logger(config.log_path + 'get_assetapplyid.log')
+        log.wirte(api, '', rep)
+
+    def tearDown(self):
+        print('====TestEnd====')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
